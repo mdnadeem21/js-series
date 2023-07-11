39a36b6925c051375f85b73059e23a4f581398f0
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-11 12:45:17 +0000

    more on conversions

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -25,5 +25,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***********************************Operations********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello "
+let str2 = "Nadeem"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(2+"2");
+// console.log("2"+2);
+// console.log("2"+"2");
+// console.log("2"+2+2);
+// console.log(2+2+"2");
+
+// console.log(+true);
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
