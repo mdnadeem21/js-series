babdadfb4d262842ed78226cc018ec1f13c20300
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-11 11:48:35 +0000

    about data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS cod as newer version
+
+
+/**
+ * premitive data type
+ * 1 - number
+ * 2 - bigint  => for large number
+ * 3 - string
+ * 4 - boolean
+ * 5 - null => standalone valu
+ * 6 - undefine
+ * 7 - symbol => unique
+ * 
+ * derive data type
+ * 1 - object
+ */
+
+console.log(typeof undefined);  // undefine
+console.log(typeof null); // object
