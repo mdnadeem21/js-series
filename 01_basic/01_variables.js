99a9c8c0f794fad3cded5aaad1bd3b64c3257ff6
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-11 08:18:51 +0000

    learn about variables and constant

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 4123
+let accoubtEmail = "ns@gmail.com"
+var accountPass =  "1234"
+accountCity = "Kolkata"
+
+
+/**
+ * prefer not to use becaue of issue in block scope and functional scope
+ * 
+*/
+console.table([accountId,accoubtEmail,accountPass,accountCity]);
