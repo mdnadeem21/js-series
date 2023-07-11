1efe3b8e47c85f119f66cd492383b2f5d4c00a95
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-11 12:13:45 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = null
+// console.log(typeof score);
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+/**
+ * "33" convert this string to a number we get => 33
+ * "33ab" convert this alphanumric string to a number we get => NaN
+ * true/false  convert this boolean value to a number we get => 1/0
+ */ 
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/**
+ * 1/0 convert this number value  to a boolean we get => true/false
+ * ""  convert this empty string to a boolean we get => false
+ * "abc" convert this string to a boolean we get => true
+ */
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
