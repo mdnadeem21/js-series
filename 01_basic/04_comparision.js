4f8b0960f4c6cf4d10ea6f7018a21436b27b448e
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-13 08:32:24 +0000

    comparision about diiferent datatypes

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,22 @@
+/**
+ * avoid  comparision of different datatypes like below
+ */
+
+// console.log("2" > 1);
+// console.log("02" > 10);
+// console.log(11 > "2");
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+console.log("2" === 2);
