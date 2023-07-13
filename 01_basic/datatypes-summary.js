c8bb90798ce809d37bdae5135afb681995c40f95
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-13 09:30:00 +0000

    about stack and heap memeroy

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -19,9 +19,9 @@
  const outSideTemp = null;
  let userEmail;
 
- console.log("Id : ",id);
- console.log("anotherId : ",anotherId);
- console.log(id === anotherId);
+//  console.log("Id : ",id);
+//  console.log("anotherId : ",anotherId);
+//  console.log(id === anotherId);
 
 
  // Reference
@@ -36,4 +36,28 @@
     console.log("Hello World");
  }
 
- console.log(typeof id);
+//  console.log(typeof id);
+
+
+ //****************************************************** */
+
+ // Memory type
+ // Stack (Primitive), Heap (Non-Primitive)
+
+ let myYoutubeName = "Nadeemdotcom"
+ let anotherName = myYoutubeName
+ anotherName = "chaiaurcode"
+
+ console.log(myYoutubeName);
+ console.log(anotherName);
+
+ let userOne = {
+    email : "abc@abc.com",
+    upi : "user1@ybl"
+ }
+ let userTwo = userOne
+
+ userTwo.email = "abc@google.com"
+
+ console.log("User Two : ",userTwo);
+ console.log("User One : ",userOne);
