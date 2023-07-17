ec06cb65f726fb3f09986ef386632f28517ccd5a
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-17 10:40:27 +0000

    about nums and maths

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 3.14
+// console.log(otherNumber.toPrecision(4));
+
+const hundres = 100000
+// console.log(hundres.toLocaleString('en-IN'));
+
+
+/**************************MATHS******************************** */
+// console.log(Math)
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4,5,1,0,8));
+// console.log(Math.max(4,5,1,0,8));
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
