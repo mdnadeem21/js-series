9747fb117d927658ea5db19f52ea8ca13a997d68
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-18 07:17:18 +0000

    about dates in js

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,26 @@
+ let myDate = new Date()
+//  console.log(myDate.toString());
+//  console.log(myDate.toISOString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleString());
+//  console.log(myDate.toLocaleTimeString());
+
+//  let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,3,3)
+// let myCreatedDate = new Date("2023-01-15")
+let myCreatedDate = new Date("01-15-2023")
+//  console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/10000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1 );
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday : "long"
+})
