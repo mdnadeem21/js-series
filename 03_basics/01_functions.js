635c5576f8e952a9464ebf4eec042055039ac9ec
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-24 13:56:32 +0000

    more about funtions with array and object as a parameter

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,4 +29,34 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Nadeem"));
+// console.log(loginUserMessage("Nadeem"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Nadeem",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
