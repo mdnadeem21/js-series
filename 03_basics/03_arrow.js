b9a1661a46922e537e657c1c6fa4f7f782ba1883
mdnadeem21 <86469018+mdnadeem21@users.noreply.github.com>
2023-07-24 14:09:38 +0000

    about arrow functions in js

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,49 @@
+const user = {
+    username: "nadeem",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "nadeem"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "nadeem"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "nadeem"
+    console.log(this);
+}
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "nadeem"})
+
+
+console.log(addTwo(3, 4))
+
